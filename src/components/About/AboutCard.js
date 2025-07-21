0ea95c34e360b51e2a44b3df4bd3ce9a8e3e561e
Agnik Dutta <agnikdutta88@gmail.com>
2025-07-21 23:17:01 +0530

    Replace SVG images with PNG images and remove unused SVG files

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Kangkana Barman </span>
-            from <span className="purple"> Assam, India.</span>
+            Hi Everyone, I am <span className="purple">Agnik Dutta</span>
+            from <span className="purple"> Kolkata, India.</span>
             <br />
             I am currently a student in computer science and engineering at SRMIST.
             <br />
@@ -23,17 +23,17 @@ function AboutCard() {
               <ImPointRight /> Start-up
             </li>
             <li className="about-activity">
-              <ImPointRight /> Book Reading
+              <ImPointRight /> Stock Market
             </li>
             <li className="about-activity">
-              <ImPointRight /> Agnik ko pelna
+              <ImPointRight /> Data Science 
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Kangkana</footer>
+          <footer className="blockquote-footer">Agnik</footer>
         </blockquote>
       </Card.Body>
     </Card>
